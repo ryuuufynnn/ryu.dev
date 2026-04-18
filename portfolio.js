bcf20fd5da52a39bbc98d2e2d449197a84051ce9
ryu.code <comp.sci.lau@programmer.net>
2026-04-19 04:46:54 +0800

    add a bg music and personal responsiveness for my device.

--- portfolio.js
+++ portfolio.js
@@ -1,2 +1,20 @@
-window.onload = () => 
+window.onload = () =>
     document.body.classList.remove('loading');
+
+// bg music
+document.addEventListener("DOMContentLoaded", () => {
+
+    const music = document.getElementById("bg-music");
+    const icon = document.getElementById("javaUwu");
+
+    icon.addEventListener("click", () => {
+        if(music.paused) {
+            music.play();
+            icon.style.color = "lime";
+        } else {
+            music.pause();
+            icon.style.color = "brown";
+        }
+    });
+
+});
